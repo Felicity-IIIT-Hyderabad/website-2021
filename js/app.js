e4122da309ac0be74ef9fae5066559ee628a0aaa
shreygupta2809 <shrey2809@gmail.com>
2021-01-26 15:30:31 +0530

    Semi-rewind

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
-const text = intro.querySelector("h1");
+const intro = document.querySelector(".replay");
+const video = document.querySelector(".replay video");
+// const text = intro.querySelector("h1");
 //END SECTION
 const section = document.querySelector("section");
 const end = section.querySelector("h1");
@@ -12,35 +12,35 @@ const controller = new ScrollMagic.Controller();
 let scene = new ScrollMagic.Scene({
   duration: 9000,
   triggerElement: intro,
-  triggerHook: 0
+  triggerHook: 0,
 })
-  .addIndicators()
+  // .addIndicators()
   .setPin(intro)
   .addTo(controller);
 
 //Text Animation
-const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+// const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
 
-let scene2 = new ScrollMagic.Scene({
-  duration: 3000,
-  triggerElement: intro,
-  triggerHook: 0
-})
-  .setTween(textAnim)
-  .addTo(controller);
+// let scene2 = new ScrollMagic.Scene({
+//   duration: 3000,
+//   triggerElement: intro,
+//   triggerHook: 0
+// })
+//   .setTween(textAnim)
+//   .addTo(controller);
 
 //Video Animation
-let accelamount = 0.1;
+let accelamount = 1;
 let scrollpos = 0;
 let delay = 0;
 
-scene.on("update", e => {
+scene.on("update", (e) => {
   scrollpos = e.scrollPos / 1000;
 });
 
 setInterval(() => {
   delay += (scrollpos - delay) * accelamount;
-  console.log(scrollpos, delay);
+  // console.log(scrollpos, delay);
 
   video.currentTime = delay;
 }, 33.3);
--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,5 @@
+let lastScroll = 0;
+
 const preloader = document.getElementById("loading");
 const main = document.getElementById("main");
 function displayContent() {
@@ -8,7 +10,7 @@ function displayContent() {
     main.style.display = "block";
     setTimeout(() => (main.style.opacity = 1), 50);
     const headerVideo = document.getElementById("header-video");
-    const normalVideo = document.getElementById("normal-video");
+    // const normalVideo = document.getElementById("normal-video");
 
     headerVideo.muted = true;
     headerVideo.playbackRate = 1.75;
@@ -16,11 +18,68 @@ function displayContent() {
     headerVideo.onended = function () {
       document.querySelector("body").style.overflowY = "scroll";
     };
-    normalVideo.muted = true;
-    normalVideo.play();
+    // normalVideo.muted = true;
+    // normalVideo.play();
     // headerVideo.onended = function () {
     //   document.querySelector("body").style.overflowY = "scroll";
     // };
   }, 40);
 }
 displayContent();
+
+const video = $(".replay video").get(0);
+
+function scrollVideo() {
+  // const video = document.querySelector(".replay").get(0);
+  // var video = document.querySelector(".replay"),
+  //   videoLength = video.duration,
+  //   scrollPosition = document.documentElement.scrollTop;
+  // console.log(video, videoLength, scrollPosition);
+
+  const scrollPosition = $(document).scrollTop();
+  const videoLength = video.duration;
+  // console.log(scrollPosition);
+
+  // console.log(video, videoLength, scrollPosition);
+
+  if (
+    scrollPosition > 1100 &&
+    (lastScroll > scrollPosition + 1 || lastScroll < scrollPosition - 1)
+  ) {
+    video.currentTime = ((scrollPosition - 1100) * videoLength) / 500;
+    console.log(video.currentTime);
+
+    lastScroll = scrollPosition;
+  }
+}
+
+$(window).scroll(function (e) {
+  scrollVideo();
+});
+
+function scrollVid() {
+  var frameNumber = 0, // start video at frame 0
+    // lower numbers = faster playback
+    playbackConst = 500,
+    // get page height from video duration
+    setHeight = document.getElementById("set-height"),
+    // select video element
+    vid = document.querySelector(".replay video");
+  // var vid = $('#v0')[0]; // jquery option
+
+  // dynamically set the page height according to video length
+  vid.addEventListener("loadedmetadata", function () {
+    setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+  });
+
+  // Use requestAnimationFrame for smooth playback
+  function scrollPlay() {
+    var frameNumber = window.pageYOffset / playbackConst;
+    vid.currentTime = frameNumber;
+    window.requestAnimationFrame(scrollPlay);
+  }
+
+  window.requestAnimationFrame(scrollPlay);
+}
+
+// scrollVid()
