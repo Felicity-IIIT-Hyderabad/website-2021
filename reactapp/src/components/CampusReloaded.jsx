901a7a0a089cdf503640c8d8a8d64aad40c601dc
v15hv4 <vishva2912@gmail.com>
2021-01-28 14:42:41 +0530

    Add vignette class

--- reactapp/src/components/CampusReloaded.jsx
+++ reactapp/src/components/CampusReloaded.jsx
@@ -2,7 +2,7 @@ import { Container } from "reactstrap";
 
 const CampusReloaded = () => {
     return (
-        <Container fluid className="full-page text-light">
+        <Container fluid className="full-page text-light vignette">
             <img src="/himalaya-static.png" alt="" className="bg-media" />
         </Container>
     );
--- reactapp/src/components/Intro.jsx
+++ reactapp/src/components/Intro.jsx
@@ -2,7 +2,7 @@ import { Container } from "reactstrap";
 
 const Intro = () => {
     return (
-        <Container fluid className="full-page text-light">
+        <Container fluid className="full-page text-light vignette">
             <video autoPlay muted className="bg-media" width="2000">
                 <source src="/videos/websitemedium1.mp4" type="video/mp4" />
             </video>
