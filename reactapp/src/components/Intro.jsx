c202f75e263e452b4b43bfaaadd3955e0ebbacc2
v15hv4 <vishva2912@gmail.com>
2021-01-28 15:00:35 +0530

    Fix intro video positioning

--- reactapp/src/components/Intro.jsx
+++ reactapp/src/components/Intro.jsx
@@ -3,7 +3,7 @@ import { Container } from "reactstrap";
 const Intro = () => {
     return (
         <Container fluid className="full-page text-light vignette">
-            <video autoPlay muted className="bg-media" width="2000">
+            <video autoPlay muted className="bg-media">
                 <source src="/videos/websitemedium1.mp4" type="video/mp4" />
             </video>
         </Container>
