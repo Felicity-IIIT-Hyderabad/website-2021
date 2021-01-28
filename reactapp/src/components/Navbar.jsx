ccd57fa29638d78cef85471b8d0d6b27c5d14f73
v15hv4 <vishva2912@gmail.com>
2021-01-28 15:52:34 +0530

    Fix scroll offsets

--- reactapp/src/components/Navbar.jsx
+++ reactapp/src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
                             activeClass="active"
                             to="about"
                             spy={true}
-                            offset={-50}
+                            offset={-80}
                             smooth={true}
                             duration={500}
                         >
@@ -42,7 +42,7 @@ const Navbar = () => {
                             activeClass="active"
                             to="campus-reloaded"
                             spy={true}
-                            offset={-50}
+                            offset={-80}
                             smooth={true}
                             duration={500}
                         >
@@ -55,7 +55,7 @@ const Navbar = () => {
                             activeClass="active"
                             to="contact"
                             spy={true}
-                            offset={-50}
+                            offset={-80}
                             smooth={true}
                             duration={500}
                         >
