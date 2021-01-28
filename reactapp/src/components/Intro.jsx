304a0968ca7710c144b02fc49223181c1b79f8d3
v15hv4 <vishva2912@gmail.com>
2021-01-28 13:44:58 +0530

    Finish Intro component

--- reactapp/src/components/Intro.jsx
+++ reactapp/src/components/Intro.jsx
@@ -3,7 +3,9 @@ import { Container } from "reactstrap";
 const Intro = () => {
     return (
         <Container fluid className="full-page text-light">
-            intro
+            <video autoPlay muted className="bg-video" width="2000">
+                <source src="/videos/websitemedium1.mp4" type="video/mp4" />
+            </video>
         </Container>
     );
 };
