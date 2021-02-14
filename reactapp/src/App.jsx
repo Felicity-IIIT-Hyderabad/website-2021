7874e572c1ff4b0b58c5e400c3d0114cc4897566
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2021-02-14 19:17:00 +0530

    fixed alignment

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
-import Sponsors from "./components/Sponsors"
+import Sponsors from "./components/Sponsors";
 import TechEvent from "./components/TechEvent";
 import CultEvent from "./components/CultEvent";
 import CPWorkshop from "./components/CPWorkshop";
@@ -12,15 +12,10 @@ const App = () => {
     return (
         <>
             <div id="main">
-                <Navbar />
                 <Router>
                     <Switch>
-		                <Route path="/sponsors">
-                          <Sponsors id="sponsors" />
-                        </Route>
-                        <Route path="/">
-                            <Intro id="home" />
-                            <Home />
+                        <Route path="/sponsors">
+                            <Sponsors id="sponsors" />
                         </Route>
                         <Route path="/workshop/cp" component={CPWorkshop} />
                         <Route path="/workshop/ml" component={MLWorkshop} />
