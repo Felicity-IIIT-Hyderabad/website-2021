1e6e0401772c0d5a1b023ff917ed8c9ebe317afc
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2021-02-17 17:31:03 +0530

    trying

--- reactapp/src/actions/login.js
+++ reactapp/src/actions/login.js
@@ -1,13 +1,10 @@
 import store from "../store";
 
 export function loginUser(keycloak){
-    console.log(keycloak);
-    console.log("ME CALLED");
     return store.dispatch({ type:"LOGIN", keycloak });
 }
 
 export function logoutUser(){
-    console.log("ME CALLED");
     return store.dispatch({ type:"LOGOUT" });
 }
 
