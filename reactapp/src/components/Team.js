451136fb7e5b2c9362b5ecc549c882a697e340ea
Prajneya <prajneya.kumar@research.iiit.ac.in>
2021-02-17 17:31:45 +0530

    Show non-main people

--- reactapp/src/components/Team.js
+++ reactapp/src/components/Team.js
@@ -132,7 +132,7 @@ const Team = () => {
                                     </div> : ""
                                 
                             ))}
-                            {teamMembers.default["events"].map((teamMember, idx) => (
+                            {teamMembers.default["web"].map((teamMember, idx) => (
                                 teamMember["main"] ? "" : 
                                     <div className="non-featured d-inline-block mx-4">
                                         <img className="img-fluid mt-5" src={teamMember["image"]} alt="Thumb" />
@@ -159,7 +159,7 @@ const Team = () => {
                                     </div> : ""
                                 
                             ))}
-                            {teamMembers.default["events"].map((teamMember, idx) => (
+                            {teamMembers.default["design"].map((teamMember, idx) => (
                                 teamMember["main"] ? "" : 
                                     <div className="non-featured d-inline-block mx-4">
                                         <img className="img-fluid mt-5" src={teamMember["image"]} alt="Thumb" />
@@ -186,7 +186,7 @@ const Team = () => {
                                     </div> : ""
                                 
                             ))}
-                            {teamMembers.default["events"].map((teamMember, idx) => (
+                            {teamMembers.default["marketing"].map((teamMember, idx) => (
                                 teamMember["main"] ? "" : 
                                     <div className="non-featured d-inline-block mx-4">
                                         <img className="img-fluid mt-5" src={teamMember["image"]} alt="Thumb" />
@@ -213,7 +213,7 @@ const Team = () => {
                                     </div> : ""
                                 
                             ))}
-                            {teamMembers.default["events"].map((teamMember, idx) => (
+                            {teamMembers.default["corporate"].map((teamMember, idx) => (
                                 teamMember["main"] ? "" : 
                                     <div className="non-featured d-inline-block mx-4">
                                         <img className="img-fluid mt-5" src={teamMember["image"]} alt="Thumb" />
