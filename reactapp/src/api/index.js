3f03d7bf6e3151081651ed9b417fd7b0ca6f3de1
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2021-02-20 11:07:24 +0530

    added api

--- reactapp/src/api/index.js
+++ reactapp/src/api/index.js
@@ -1,2 +1,3 @@
 export var eventsTechnicalApi = "https://felicity.iiit.ac.in/events-technical";
 export var eventsCulturalApi = "https://felicity.iiit.ac.in/events-cultural";
+export var eventsApi = "https://felicity.iiit.ac.in/events";
