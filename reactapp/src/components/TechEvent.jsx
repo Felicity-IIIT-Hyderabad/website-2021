7513aefc5449c72b8336d1bd83020753c7f15816
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2021-02-22 11:32:49 +0530

    added catch

--- reactapp/src/components/TechEvent.jsx
+++ reactapp/src/components/TechEvent.jsx
@@ -143,7 +143,8 @@ class TechEvent extends React.Component {
     }
 
     getRegisteredEvents(){
-        axios.get(eventsRegisteredApi, {"Authorization":JSON.parse(window.localStorage.getItem("user")).token}
+        axios.get(eventsRegisteredApi,{},  {
+            header: {"Authorization":JSON.parse(window.localStorage.getItem("user")).token}}
         ).then(async (res)=>{
             this.setState({
                 myEvents: res.data
